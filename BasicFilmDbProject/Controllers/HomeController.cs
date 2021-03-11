696c0d8c104acc19334026509a4abcb1167769e0
LabStudent-55-504266 <LabStudent-55-504266@ML-RefVm-41417>
2021-03-11 22:38:10 +0000

    Completed project

--- BasicFilmDbProject/Controllers/HomeController.cs
+++ BasicFilmDbProject/Controllers/HomeController.cs
@@ -15,12 +15,77 @@ namespace BasicFilmDbProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
+        public IActionResult OneFilm()
+
+        {
+
+            Film model = _context.Films.FirstOrDefault();
+
+            return View(model);
+
+        }
+
+        public IActionResult AllFilms()
+
+        {
+
+            List<Film> model = _context.Films.ToList();
+
+            return View(model);
+
+        }
+
+        public IActionResult FilmDetails(int id)
+
+        {
+
+            Film model = _context.Films.Find(id);
+
+            return View(model);
+
+        }
+
+        public IActionResult Search(String SearchString)
+
+        {
+
+            if (!string.IsNullOrEmpty(SearchString))
+
+            {
+
+
+                var films = from m in _context.Films
+
+                            where m.FilmTitle.Contains(SearchString)
+
+                            select m;
+
+                List<Film> model = films.ToList();
+
+                ViewData["SearchString"] = SearchString;
+
+                return View(model);
+
+            }
+            else
+            {
+                return View();
+            }
+
+
+
+        }
+
+
 
 
         public IActionResult Index()
